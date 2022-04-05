0a7ed62a7259cfe2ff94cf06fca5358430a2441c
Silvio REA <silvio.rea07@gmail.com>
2022-04-05 16:36:17 +0200

    MAJ diagramms and readMe

--- docs/paymybuddy_Script_Data.sql
+++ docs/paymybuddy_Script_Data.sql
@@ -16,13 +16,33 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Dumping data for table `charges`
+--
+
+LOCK TABLES `charges` WRITE;
+/*!40000 ALTER TABLE `charges` DISABLE KEYS */;
+INSERT INTO `charges` VALUES (1,0.95,5,1),(2,0.95,1.25,2),(3,0.95,0.10000000000000009,3),(4,0.95,1.25,4),(5,0.95,5,5),(6,0.95,0.5099999999999998,6);
+/*!40000 ALTER TABLE `charges` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Dumping data for table `contact`
 --
 
 LOCK TABLES `contact` WRITE;
 /*!40000 ALTER TABLE `contact` DISABLE KEYS */;
-INSERT INTO `contact` VALUES (1,2),(1,3);
+INSERT INTO `contact` VALUES (1,2);
 /*!40000 ALTER TABLE `contact` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `credit`
+--
+
+LOCK TABLES `credit` WRITE;
+/*!40000 ALTER TABLE `credit` DISABLE KEYS */;
+INSERT INTO `credit` VALUES (1,100,1),(2,10.5,1),(3,100,1),(4,100,1),(5,500,1),(6,100,1),(7,100,1);
+/*!40000 ALTER TABLE `credit` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -31,7 +51,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `operation` WRITE;
 /*!40000 ALTER TABLE `operation` DISABLE KEYS */;
-INSERT INTO `operation` VALUES (1,33.25,'birthday',2,1),(2,9.975,'cinema',3,1);
+INSERT INTO `operation` VALUES (1,95,'perso',2,1),(2,23.75,'',2,1),(3,1.9,'',2,1),(4,23.75,'',2,1),(5,95,'',2,1),(6,9.69,'',2,1);
 /*!40000 ALTER TABLE `operation` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -41,7 +61,7 @@ UNLOCK TABLES;
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,454.5,'user1@mail.com','USER 1','user 1','$2a$10$dQdejdyjTgTLoprfv50vMenX5BcUxC7pWyEnKrmYPhlWgvTkkRotS','USER'),(2,33.25,'user2@mail.com','USER2','user2','$2a$10$5z.T7GX2P/ACHBkV05/yfuyYQfMkgNzLOo/crsrSi3dWl8Cbuxe86','USER'),(3,9.975,'user3@mail.com','USER3','user3','$2a$10$qraGfXO0sG0qFFgvLLw1MuXhOf2ZD3AjGtr1koClhSxs0d8rR8bo6','USER');
+INSERT INTO `user` VALUES (1,748.3,'user1@mail.com','USER 1','user','$2a$10$Rwsvh.kL3/mN3lbWUUleLe3BGRttTvL3wo.2ct8M4ip8bAepLi1L.','USER'),(2,249.09,'user2@mail.com','USER2','user','$2a$10$SxKOCDZgvJQlf/N2D90s8eLOl2Kf2T9K9wnJB39kbUvYv6Nx.ih7O','USER'),(3,0,'user3@mail.com','USER3','user','$2a$10$pZ8rIu1T1MDfM3culHX3MOfOTvgIzYIpvc2R4J5IEIQBMIFzUEtpW','USER');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -54,4 +74,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-30 11:48:42
+-- Dump completed on 2022-04-05 16:25:54
--- docs/paymybuddy_Script_Structure.sql
+++ docs/paymybuddy_Script_Structure.sql
@@ -16,6 +16,24 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `charges`
+--
+
+DROP TABLE IF EXISTS `charges`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `charges` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `charges` double NOT NULL,
+  `charge` double DEFAULT NULL,
+  `operation` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKskvisyt7w2ntpmddnbx2u9077` (`operation`),
+  CONSTRAINT `FKskvisyt7w2ntpmddnbx2u9077` FOREIGN KEY (`operation`) REFERENCES `operation` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `contact`
 --
 
@@ -30,6 +48,23 @@ CREATE TABLE `contact` (
   CONSTRAINT `FKe07k4jcfdophemi6j1lt84b61` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   CONSTRAINT `FKr46b7oluerb9faqnbjaaqsea` FOREIGN KEY (`contact_user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `credit`
+--
+
+DROP TABLE IF EXISTS `credit`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `credit` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `amount` double DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKruqa8y0rd9lldy1mcwq78shlv` (`user_id`),
+  CONSTRAINT `FKruqa8y0rd9lldy1mcwq78shlv` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -50,7 +85,7 @@ CREATE TABLE `operation` (
   KEY `FKgqsmrr1j982vdj453jrqrdm19` (`sender`),
   CONSTRAINT `FKgqsmrr1j982vdj453jrqrdm19` FOREIGN KEY (`sender`) REFERENCES `user` (`id`),
   CONSTRAINT `FKqj9oghasl2shmjgpttbymiyjk` FOREIGN KEY (`receiver`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -82,4 +117,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-30 11:45:12
+-- Dump completed on 2022-04-05 16:26:39
